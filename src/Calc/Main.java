0146008c9fe745278e852c3f51cc973bc82fdbf2
LuxeAo <Vlad122131@gmail.com>
2024-07-17 14:13:55 +0400

    refactor

--- src/Calc/Main.java
+++ src/Calc/Main.java
@@ -1,12 +1,40 @@
 package Calc;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
+    private static final Map<String, Integer> romanToArabic = new HashMap<>();
+    private static final Map<Integer, String> arabicToRoman = new HashMap<>();
+
+    static {
+        romanToArabic.put("I", 1);
+        romanToArabic.put("II", 2);
+        romanToArabic.put("III", 3);
+        romanToArabic.put("IV", 4);
+        romanToArabic.put("V", 5);
+        romanToArabic.put("VI", 6);
+        romanToArabic.put("VII", 7);
+        romanToArabic.put("VIII", 8);
+        romanToArabic.put("IX", 9);
+        romanToArabic.put("X", 10);
+
+        arabicToRoman.put(100, "C");
+        arabicToRoman.put(90, "XC");
+        arabicToRoman.put(50, "L");
+        arabicToRoman.put(40, "XL");
+        arabicToRoman.put(10, "X");
+        arabicToRoman.put(9, "IX");
+        arabicToRoman.put(5, "V");
+        arabicToRoman.put(4, "IV");
+        arabicToRoman.put(1, "I");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Введите матемаматический пример в формате:число;операция;число. \n" +
-                "Калькулятор работает только с арабскими,либо с римскими числами. \n" +
+        System.out.println("Введите матемаматический пример в формате: число операция число. \n" +
+                "Калькулятор работает только с арабскими, либо с римскими числами. \n" +
                 "Калькулятор работает только с целыми числами");
         Scanner scanner = new Scanner(System.in);
         String inputData = scanner.nextLine(); // Считывание примера с консоли
@@ -16,90 +44,69 @@ public class Main {
     }
 
     public static String calc(String inputData) throws Exception {
-        String[] strings = inputData.split(" "); //Разбиение массива с пробелами
-        int number_1, number_2, resultCalc = 0;
-        String operation, Number_1, Number_2, Result = "";
-        if (strings.length > 3) {
-            throw new Exception("введите пример в формате число;операция;число"); //Исключение,пример содержит больше 3 аргументов
-        } else if (strings.length < 3) {
-            throw new Exception("строка не является математическим примером"); //Исключение,пример содержит меньше 3 аргументов
+        String[] strings = inputData.split(" "); // Разбиение массива с пробелами
+        if (strings.length != 3) {
+            throw new Exception("Введите пример в формате число операция число");
         }
-        operation = strings[1];
-        Number_1 = strings[0];
-        Number_2 = strings[2];
-        try {
-            number_1 = Integer.parseInt(Number_1);
-            number_2 = Integer.parseInt(Number_2);
-            if (number_1 < 1 || number_1 > 10 || number_2 < 1 || number_2 > 10) { //Проверка на соответствие диапазону от 1 до 10
-                throw new Exception("Арабские числа не входят в диапазон [1,10]");
-            } else {
-                switch (operation) {
-                    case "+" -> resultCalc = number_1 + number_2;
-                    case "-" -> resultCalc = number_1 - number_2;
-                    case "*" -> resultCalc = number_1 * number_2;
-                    case "/" -> resultCalc = number_1 / number_2;
-                    default -> {
-                        throw new Exception("арифметический знак не является  +/-/*//"); //Введеная операция не соответствует требуемым
-                    }
-                }
-            }
-            System.out.println(resultCalc);
-        } catch (NumberFormatException e) {
-            switch (Number_1) {
-                case "I" -> number_1 = 1;
-                case "II" -> number_1 = 2;
-                case "III" -> number_1 = 3;
-                case "IV" -> number_1 = 4;
-                case "V" -> number_1 = 5;
-                case "VI" -> number_1 = 6;
-                case "VII" -> number_1 = 7;
-                case "VIII" -> number_1 = 8;
-                case "IX" -> number_1 = 9;
-                case "X" -> number_1 = 10;
-                default -> {
-                    throw new Exception("смешанные числа");
-                }
-            }
-            switch (Number_2) {
-                case "I" -> number_2 = 1;
-                case "II" -> number_2 = 2;
-                case "III" -> number_2 = 3;
-                case "IV" -> number_2 = 4;
-                case "V" -> number_2 = 5;
-                case "VI" -> number_2 = 6;
-                case "VII" -> number_2 = 7;
-                case "VIII" -> number_2 = 8;
-                case "IX" -> number_2 = 9;
-                case "X" -> number_2 = 10;
-                default -> {
-                    throw new Exception("смешанный числа");
-                }
-            }
-            switch (operation) {
-                case "+" -> resultCalc = number_1 + number_2;
-                case "-" -> resultCalc = number_1 - number_2;
-                case "*" -> resultCalc = number_1 * number_2;
-                case "/" -> resultCalc = number_1 / number_2;
-                default -> {
-                    throw new Exception("арифметический знак не явл. +/-/*//");
-                }
-            }
-            if (resultCalc < 1) {
-                throw new Exception("римские числа не могут быть < 1");
-            }
-            int[] num = {100, 90, 50, 40, 10, 9, 5, 4, 1};
-            String[] figure = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-            Result = "";
-            for (int i = 0; i < num.length; i++) {
-                while (resultCalc >= num[i]) {
-                    Result = Result + figure[i];
-                    resultCalc = resultCalc - num[i];
-                }
-            }
+
+        String operation = strings[1];
+        String number1Str = strings[0];
+        String number2Str = strings[2];
+
+        int number1 = parseNumber(number1Str);
+        int number2 = parseNumber(number2Str);
+
+        if (number1 < 1 || number1 > 10 || number2 < 1 || number2 > 10) {
+            throw new Exception("Числа должны быть в диапазоне от 1 до 10");
         }
-        return Result;
+
+        int result = performOperation(number1, number2, operation);
+
+        if (isRoman(number1Str) && isRoman(number2Str)) {
+            if (result < 1) {
+                throw new Exception("Римские числа не могут быть меньше 1");
+            }
+            return convertToRoman(result);
+        } else {
+            return String.valueOf(result);
+        }
     }
 
+    private static int parseNumber(String numberStr) throws Exception {
+        try {
+            return Integer.parseInt(numberStr);
+        } catch (NumberFormatException e) {
+            Integer value = romanToArabic.get(numberStr);
+            if (value == null) {
+                throw new Exception("Некорректное число: " + numberStr);
+            }
+            return value;
+        }
+    }
 
+    private static int performOperation(int number1, int number2, String operation) throws Exception {
+        return switch (operation) {
+            case "+" -> number1 + number2;
+            case "-" -> number1 - number2;
+            case "*" -> number1 * number2;
+            case "/" -> number1 / number2;
+            default -> throw new Exception("Некорректная операция: " + operation);
+        };
+    }
+
+    private static boolean isRoman(String numberStr) {
+        return romanToArabic.containsKey(numberStr);
+    }
+
+    private static String convertToRoman(int number) {
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<Integer, String> entry : arabicToRoman.entrySet()) {
+            while (number >= entry.getKey()) {
+                result.append(entry.getValue());
+                number -= entry.getKey();
+            }
+        }
+        return result.toString();
+    }
 }
 
